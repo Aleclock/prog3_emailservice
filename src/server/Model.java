341f37a01d12ece78e8169af18116067d6bca5be
Alessandro Clocchiatti <alessandro.clocchiat@edu.unito.it>
2021-03-20 19:22:59 +0100

    Create basic structure

---
+++ src/server/Model.java
@@ -0,0 +1,15 @@
+package server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Model extends Application {
+  @Override
+  public void start(Stage stage) throws Exception {
+
+  }
+
+  public static void main (String[] args) {
+    launch(args);
+  }
+}
