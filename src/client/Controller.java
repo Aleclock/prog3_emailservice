d853bdd1239208a092686465341beb0fd41881dd
Alessandro Clocchiatti <alessandro.clocchiat@edu.unito.it>
2021-03-22 00:11:59 +0100

    Added Client scene and Controller

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,4 +1,42 @@
 package client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+  Model model;
+
+  @FXML
+  private TextField tf_email;
+  @FXML
+  private Label label_error;
+
+  public void initModel(Model model) {
+    this.model = model;
+  }
+
+  @FXML
+  public void handleLoginButton(ActionEvent event) {
+    if (matchesEmailFormat(tf_email.getText())) {
+      System.out.println("bravoo");
+      // TODO open email page
+      // TODO open connection with server
+    } else {
+      System.out.println("errore");
+      label_error.setText("Please enter a valid email address");
+    }
+  }
+
+  @FXML
+  public void handleTextFieldSelection(MouseEvent event) {
+    label_error.setText("");
+  }
+
+  private boolean matchesEmailFormat(String email) {
+    String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+    return email.matches(regex);
+  }
 }
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,12 +1,24 @@
 package client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+  Controller controller;
+
   @Override
   public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("res/login_scene.fxml"));
+    Parent root = loader.load();
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
 
+    controller = loader.getController();
+    controller.initModel(new Model());
   }
 
   public static void main (String[] args) {
